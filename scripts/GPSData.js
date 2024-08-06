8354b0454f6bfd5e095f1dee420f670df5bf8f1f
Hector Valdivia <hect.valdivia@gmail.com>
2024-08-06 17:40:45 -0600

    Fix: Get Lat and long data

--- scripts/GPSData.js
+++ scripts/GPSData.js
@@ -1,61 +1,53 @@
-var ArduinoIotClient = require('@arduino/arduino-iot-client');
-var defaultClient = ArduinoIotClient.ApiClient.instance;
-const axios = require("axios").default
+import axios from 'axios';
 
-const CORS_PROXY = 'https://cors-anywhere.herokuapp.com/';
-const API_URL = 'https://api2.arduino.cc/iot/v2/things/a69b9f59-4fec-4123-beef-ba978f9f8a54/properties/4a032658-0771-4aa5-8483-3ed39defc59d';
+const CLIENT_ID = 'vbp7UQ6MJVoQQMClqaMJjlOSfCqGAErS';
+const CLIENT_SECRET = 'i5AbtdXktpvoETUmDdusTc5b3hknccM66M2fv9aGxeikkk3ZJuBQOOgHA7bT82Fw';
+const API_BASE_URL = 'http://159.223.182.187:3000/api';
 
 async function getToken() {
-    const url = 'https://api2.arduino.cc/iot/v1/clients/token';
     const data = new URLSearchParams({
         grant_type: 'client_credentials',
-        client_id: 'vbp7UQ6MJVoQQMClqaMJjlOSfCqGAErS',
-        client_secret: 'i5AbtdXktpvoETUmDdusTc5b3hknccM66M2fv9aGxeikkk3ZJuBQOOgHA7bT82Fw',
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
         audience: 'https://api2.arduino.cc/iot'
-    }).toString();
-
-    const config = {
-        method: 'post',
-        url: CORS_PROXY + url,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        data: data
-    };
+    });
 
     try {
-        const response = await axios(config);
-        console.log("Token obtenido correctamente: " + response.data['access_token']);
-        return response.data['access_token'];
+        const response = await axios.post(
+            `${API_BASE_URL}/iot/v1/clients/token`,
+            data,
+        );
+
+        console.log("Token obtenido correctamente: " + ['access_token']);
+        return axios.create({
+            baseURL: API_BASE_URL, // Replace with your base URL
+            headers: {
+                'Authorization': `Bearer ${response.data.access_token}`,
+                'Content-Type': 'application/json',
+            }
+        });
     } catch (error) {
         console.error("Failed getting an access token: " + error);
         return null;
     }
 }
 
-async function getProperty(pid, token) {
-    const API_URL = `https://api2.arduino.cc/iot/v2/things/a69b9f59-4fec-4123-beef-ba978f9f8a54/properties/${pid}`;
-    const response = await axios.get(CORS_PROXY + API_URL, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-        },
-    });
+async function getProperty(pid, authAxios) {
+    const response = await authAxios.get(`${API_BASE_URL}/iot/v2/things/a69b9f59-4fec-4123-beef-ba978f9f8a54/properties/${pid}`);
     return response.data.last_value;
 }
 
 async function getGPSData() {
     try {
-        const token = await getToken();
-        if (!token) {
-            return;
-        }
+        const authAxios = await getToken();
 
         const pidLatitude = "f4dd54d2-586a-4c0b-938a-d79491d8d80a"; // The id of the thing Latitude
         const pidLongitude = "7b8f03a7-4eb4-4acd-b58e-2f0373d17069"; // The id of the thing Longitude
 
+
         const [latitude, longitude] = await Promise.all([
-            getProperty(pidLatitude, token),
-            getProperty(pidLongitude, token),
+            getProperty(pidLatitude, authAxios),
+            getProperty(pidLongitude, authAxios),
         ]);
 
         return [latitude, longitude];
